923ddceeea03c9a68aa9cabbbfe92e82ba90c960
Niklao <vassudev.shetye@gmail.com>
2014-04-20 21:40:21 +0530

    Initio Spell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Egyan::Application.config.secret_key_base = 'f0f208f97406a9455a6bc3fa592ee2ad2c5df118599218cb917cf8abc049c62b0a29fafe5b7c4f3c662453902c8489dcfabb1c71060d2ff0e4b51adba5c1cf6f'
