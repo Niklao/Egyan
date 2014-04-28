ddabce4de0041c6fe2b7ea9afb8670a42868d113
Niklao <vassudev.shetye@gmail.com>
2014-04-28 19:54:53 +0530

    mess_1_day2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Egyan::Application.routes.draw do
+  get "register/author"
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
