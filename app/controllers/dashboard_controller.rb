d061c8d4235e7960eadeaba173f99b081f2822d8
Niklao <vassudev.shetye@gmail.com>
2014-05-16 17:47:46 +0530

    Login secure

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
   def index
+    @user=cookies[:user]
+    if @user
+      @user_id=User.get_user( @user )
+    else
+      redirect_to login_path
+    end
+   #@user = User.find_by( email_id: "#{user_id}" ) 
+ 
+   #@first_name=@user.first_name
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  
-  
+  validates :email_id , uniqueness: true
   
   
   
@@ -10,10 +9,16 @@ class User < ActiveRecord::Base
   def self.authenticate( email , password ) 
     user=User.find_by(email_id: "#{email}" ) 
     if user.password == password
-      user
+      user.email_id
     else 
       nil
     end
+  end
+  
+  def self.get_user( email ) 
+    user=User.find_by(email_id: "#{email}" ) 
+    email=user.first_name
+    return email
   end
 
 end
